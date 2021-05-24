0d814e95cee1a7365f10834f481449ddd9d4b62d
Tchaikousky Thomas <tchaikouskythomas@gmail.com>
2021-05-24 11:49:58 -0400

    TT: initial commit

---
+++ src/main/java/com/tchaikouskiy/JunitExample.java
@@ -0,0 +1,10 @@
+package com.tchaikouskiy;
+
+public class JunitExample {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/test/java/com/tchaikousky/JunitExampleTest.java
@@ -0,0 +1,17 @@
+package com.tchaikousky;
+
+import org.junit.jupiter.api.Test;
+
+public class JunitExampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Test
+	public void TestMethod() {
+		
+	}
+
+}
